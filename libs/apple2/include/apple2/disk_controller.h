a1ca5c93f008981cffa94bd55c4645621b4196ad
Jason Stewart <jason@sketchylizard.com>
2025-09-25 09:07:36 -0400

    Add track information to DiskController

--- libs/apple2/include/apple2/disk_controller.h
+++ libs/apple2/include/apple2/disk_controller.h
@@ -27,13 +27,47 @@ public:
   void write(Address address, Address normalizedAddress, Byte data) override;
 
 private:
+  // All controller cards use ROM memory in the range of $C0n0-$C0nF where n is the slot number.
+  // The disk controller is typically in slot 6, so its ROM is at $C600-$C6FF.
+  // The slot's corresponding I/O space is at $C0x0-$C0xF, where x = slot number + 8.
+  static constexpr Address c_slot1Rom{0xC100};
+
+  enum class Control : Byte
+  {
+    Phase0_Off = 0,
+    Phase0_On = 1,
+    Phase1_Off = 2,
+    Phase1_On = 3,
+    Phase2_Off = 4,
+    Phase2_On = 5,
+    Phase3_Off = 6,
+    Phase3_On = 7,
+    Motor_Off = 8,
+    Motor_On = 9,
+    SelectDrive0 = 0x0A,
+    SelectDrive1 = 0x0B,
+    Q6Low = 0x0C,
+    Q6High = 0x0D,
+    Q7Low = 0x0E,
+    Q7High = 0x0F
+  };
+
+  static constexpr Byte Phase0Mask{0x01};
+  static constexpr Byte Phase1Mask{0x02};
+  static constexpr Byte Phase2Mask{0x04};
+  static constexpr Byte Phase3Mask{0x08};
+  static constexpr Byte MotorMask{0x10};
+  static constexpr Byte DriveSelectMask{0x20};
+  static constexpr Byte Q6Mask{0x40};
+  static constexpr Byte Q7Mask{0x80};
+
   static Byte c_rom[256];
 
   std::vector<Byte> m_diskData;
   bool m_diskLoaded = false;
-  int32_t m_bytesToRead = 0;
-  int32_t m_currentTrack = 0;
-  int32_t m_currentSector = 0;
+  mutable Byte m_status = 0x00;
+  mutable Byte m_lastPhase = 0x00;
+  mutable int8_t m_track = 34 * 2;  // Actually half track (0-68)
 
   Byte readCurrentSector() const;
 };
--- libs/apple2/src/disk_controller.cpp
+++ libs/apple2/src/disk_controller.cpp
@@ -1,5 +1,6 @@
 #include "apple2/disk_controller.h"
 
+#include <bit>
 #include <iostream>
 #include <sstream>
 
@@ -9,6 +10,19 @@ namespace apple2
 {
 using Common::Address;
 using Common::Byte;
+
+namespace
+{
+std::array<int8_t, 256> c_stepTable = []()
+{
+  std::array<int8_t, 256> table{};
+  table.fill(0);
+  table[0x18] = table[0x84] = table[0x42] = table[0x21] = -1;  // Move outward
+  table[0x81] = table[0x12] = table[0x24] = table[0x48] = 1;  // Move inward
+  return table;
+}();
+
+}  // namespace
 
 Byte DiskController::c_rom[256] = {};
 
@@ -30,62 +44,87 @@ bool DiskController::loadDisk(const std::string& filename)
   m_diskData.resize(143360);  // Standard .dsk size
   file.read(reinterpret_cast<char*>(m_diskData.data()), 143360);
   m_diskLoaded = true;
-  m_bytesToRead = 256;
   return true;
 }
 
 Common::Byte DiskController::read(Address address, Address normalizedAddress) const
 {
-  // There are two ranges that map to the disk controller: $C0E0-$C0EF and $C600-$C6FF.
-  // The second range will be normalized, so they should all be between 0x00 and 0xFF.
-  if (static_cast<size_t>(normalizedAddress) < std::size(c_rom))
-  {
-    return c_rom[static_cast<size_t>(normalizedAddress)];
-  }
+  // Disk controller ROM is mapped at $C600-$C6FF
+  // Control/status registers are at $C0E0-$C0EF
 
-  switch (static_cast<size_t>(address))
+  if (address < c_slot1Rom)
   {
-    case 0x0C:  // Read data
-      return readCurrentSector();
-    case 0x0D:  // Write protect (return not write protected)
-      return 0x00;
-    case 0x0E:  // Drive status
-      return m_diskLoaded && (m_bytesToRead > 0) ? 0x00 : 0x80;
-    default: return 0x00;
+    // Control/status registers
+    Control control{static_cast<Byte>(normalizedAddress)};
+    switch (control)
+    {
+      case Control::Phase0_Off: m_status &= ~Phase0Mask; break;
+      case Control::Phase0_On: m_status |= Phase0Mask; break;
+      case Control::Phase1_Off: m_status &= ~Phase1Mask; break;
+      case Control::Phase1_On: m_status |= Phase1Mask; break;
+      case Control::Phase2_Off: m_status &= ~Phase2Mask; break;
+      case Control::Phase2_On: m_status |= Phase2Mask; break;
+      case Control::Phase3_Off: m_status &= ~Phase3Mask; break;
+      case Control::Phase3_On: m_status |= Phase3Mask; break;
+      case Control::Motor_Off: m_status &= ~MotorMask; break;
+      case Control::Motor_On: m_status |= MotorMask; break;
+      case Control::SelectDrive0: m_status &= ~DriveSelectMask; break;
+      case Control::SelectDrive1: m_status |= DriveSelectMask; break;
+      case Control::Q6Low: m_status &= ~Q6Mask; break;
+      case Control::Q6High: m_status |= Q6Mask; break;
+      case Control::Q7Low: m_status &= ~Q7Mask; break;
+      case Control::Q7High: m_status |= Q7Mask; break;
+    }
+    if (m_status & MotorMask)
+    {
+      auto currentPhase = static_cast<uint8_t>(m_status & 0x0f);
+      if (std::has_single_bit(currentPhase))
+      {
+        auto action = static_cast<Byte>((m_lastPhase << 4) | currentPhase);
+        auto step = c_stepTable[action];
+        m_track = std::clamp<int8_t>(static_cast<int8_t>(m_track + step), 0, 68);
+        std::stringstream stream;
+        stream << std::hex << static_cast<int>(action) << std::dec << " : " << static_cast<int>(step)
+               << " track: " << static_cast<int>(m_track) << "\n";
+        LOG(stream.str());
+        m_lastPhase = currentPhase;
+      }
+    }
+    return m_status;
+  }
+  else
+  {
+    size_t index = static_cast<size_t>(normalizedAddress);
+    assert(index < std::size(c_rom));
+    return c_rom[index];
   }
 }
 
-void DiskController::write(Address address, Address normalizedAddress, Common::Byte data)
+void DiskController::write(Address address, Address normalizedAddress, Common::Byte /*data*/)
 {
-  if (normalizedAddress < Address{0xC0E0})
-    return;
-
-  uint16_t offset = static_cast<uint16_t>(normalizedAddress) - 0xC0E0;
-
-  std::stringstream stream;
-  stream << "DiskController::write($" << std::hex << static_cast<uint16_t>(address) << ", " << data << std::dec << ")\n";
-  LOG(stream.str());
-
-  switch (offset)
+  if (address < c_slot1Rom)
   {
-    case 0x01:
-    case 0x03:
-    case 0x05:
-    case 0x07:  // Track selection
-      m_currentTrack = (offset >> 1);
-      break;
-    case 0x09:
-    case 0x0B:  // Sector selection
-      m_currentSector = ((offset - 9) >> 1);
-      break;
+    size_t offset = static_cast<size_t>(normalizedAddress);
+    switch (offset)
+    {
+      case 0x01:
+      case 0x03:
+      case 0x05:
+      case 0x07:  // Track selection
+        break;
+      case 0x09:
+      case 0x0B:  // Sector selection
+        break;
+    }
+  }
+  else
+  {
+    // ROM area - ignore writes
   }
 }
 
 Common::Byte DiskController::readCurrentSector() const
 {
-  if (!m_diskLoaded || m_bytesToRead <= 0)
-    return 0;
-
   return 0;
 }
 
